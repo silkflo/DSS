d627decbd86f4515c5ce91dae4064bfe9eb68da4
silkflo <florian.assous@hotmail.fr>
2021-02-14 19:12:32 +0200

    ordersresultcopy.bat

--- Initialization/OrdersResultsCopy.bat
+++ Initialization/OrdersResultsCopy.bat
@@ -5,12 +5,18 @@ rem Script to Sync Files from Development Terminal to Version Control
 setlocal enabledelayedexpansion
 
 :: Source Directory where OrderResults is located
-:: set SOURCE_DIR1="C:\Program Files (x86)\MT4 - Terminal 1\MQL4\Files\OrdersResultsT1"
-set SOURCE_DIR2="C:\Program Files (x86)\MT4 - Terminal 2\MQL4\Files\OrdersResultsT2"
+set SOURCE_DIR1="C:\Program Files (x86)\MT4 - Terminal 1\MQL4\Files"
+set SOURCE_DIR2="C:\Program Files (x86)\MT4 - Terminal 2\MQL4\Files"
+set SOURCE_DIR3="C:\Program Files (x86)\MT4 - Terminal 3\MQL4\Files"
+set SOURCE_DIR4="C:\Program Files (x86)\MT4 - Terminal 4\MQL4\Files"
+set SOURCE_DIR5="C:\Program Files (x86)\MT4 - Terminal 5\MQL4\Files"
 :: Destination Directory where Version Control Repository is located
-:: set DEST_DIR1="C:\DSS\_DATA\OrdersResultsT1" 
-set DEST_DIR2="C:\DSS\_DATA\OrdersResultsT2"
+set DEST_DIR="C:\DSS\_DATA" 
+
 
 :: Copy only files with *.csv extension
-:: ROBOCOPY %SOURCE_DIR1% %DEST_DIR1% *.csv
-ROBOCOPY %SOURCE_DIR2% %DEST_DIR2% *.csv
+ROBOCOPY %SOURCE_DIR1% %DEST_DIR% *OrdersResultsT1.csv
+ROBOCOPY %SOURCE_DIR2% %DEST_DIR% *OrdersResultsT2.csv
+ROBOCOPY %SOURCE_DIR3% %DEST_DIR% *OrdersResultsT3.csv
+ROBOCOPY %SOURCE_DIR4% %DEST_DIR% *OrdersResultsT4.csv
+ROBOCOPY %SOURCE_DIR5% %DEST_DIR% *OrdersResultsT5.csv
